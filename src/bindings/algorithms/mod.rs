8420b3977efb1948007ad1709450092b7d150251
Ondřej Hložek <hlozek.o1@gmail.com>
2025-04-10 11:45:21 +0200

    Added initial setup for Percolation

--- src/bindings/algorithms/mod.rs
+++ src/bindings/algorithms/mod.rs
@@ -2,15 +2,19 @@ use pyo3::{types::PyModule, Bound, PyResult};
 
 pub mod cancellation;
 pub mod fixed_points;
+pub mod percolation;
 pub mod reachability;
 pub mod regulation_constraint;
 pub mod trap_spaces;
 
 pub fn register(module: &Bound<'_, PyModule>) -> PyResult<()> {
     pyo3_log::init();
-    reachability::register(module)?;
+
     fixed_points::register(module)?;
-    trap_spaces::register(module)?;
+    percolation::register(module)?;
+    reachability::register(module)?;
     regulation_constraint::register(module)?;
+    trap_spaces::register(module)?;
+
     Ok(())
 }
